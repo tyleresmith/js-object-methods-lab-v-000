93ac6c58fa606606d66fb581f38d0f5f11de1004
Tyler Smith <oats000@hotmail.com>
2016-05-29 10:38:48 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 // code solution here
+//
+// function User (name, email){
+//   this.name = name;
+//   this.email = email;
+// }
+//
+// User.prototype.sayHello = function(){
+//   console.log("Hello, my name is "+ this.name);
+// }
+//
+// kevin = new User("kevin", "kevin@aol.com");
+//
+// kevin.sayHello();
+
+function President(name, politicalParty, yearsInOffice, homeState) {
+  this.name = name;
+  this.politicalParty = politicalParty;
+  this.yearsInOffice = yearsInOffice;
+  this.homeState = homeState;
+}
+
+President.prototype.veto = function(){
+  return "NO!";
+}
+
+President.prototype.passBill = function(){
+  return "You can do that!";
+}
+
+President.prototype.doCharity = function(){
+  return "I like to help people.";
+}
+
+President.prototype.conductPressInterview = function(){
+  return "I am proud to be an American.";
+}
+
+President.prototype.sayHi = function(){
+  return "Hi, my name is " + this.name + ". I am from " + this.homeState +
+  ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice +".";
+}
+
+var georgeWashington = new President("George Washington", "Democrat", "?-?", "Delware");
+var abrahamLincoln = new President("Abraham Lincoln", "Democrat", "?-?", "North Carolina");
+var richardNixon = new President("Richard Nixon", "Republican", "?-?", "Texas");
+var jimmyCarter = new President("Jimmy Carter", "Republian", "?-?", "Hollywood");
